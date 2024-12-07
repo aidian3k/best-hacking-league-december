07deb301abd96b2c4291e681148e633e2a763887
Dawid Dobosz <doboszdav@gmail.com>
2024-12-08 00:36:35 +0100

    bodiesQueries

--- space-explorer/api/query/bodies/bodiesQueries.ts
+++ space-explorer/api/query/bodies/bodiesQueries.ts
@@ -4,10 +4,9 @@ import {useQuery} from "react-query";
 
 const astronomyApiService = axios.create({
     baseURL: process.env.EXPO_PUBLIC_ASTRONOMY_API_URL,
-    auth: {
-        username: process.env.EXPO_PUBLIC_ASTRONOMY_API_USERNAME!,
-        password: process.env.EXPO_PUBLIC_ASTRONOMY_API_PASSWORD!
-    },
+    headers:{
+        Authorization: `Basic ${btoa(`${process.env.EXPO_PUBLIC_ASTRONOMY_API_USER}:${process.env.EXPO_PUBLIC_ASTRONOMY_API_PASSWORD}`)}`
+    }
 });
 
 export const useGetBodiesPositions = (latitude: number, longitude: number) => {
@@ -16,7 +15,7 @@ export const useGetBodiesPositions = (latitude: number, longitude: number) => {
         queryFn: async () => {
             const currentDateTime: string = new Date().toISOString();
             const currentDate = currentDateTime.substring(0, currentDateTime.indexOf("T"));
-            const currentTime = currentDateTime.substring(currentDateTime.indexOf("T") + 1);
+            const currentTime = currentDateTime.substring(currentDateTime.indexOf("T") + 1, currentDateTime.indexOf("."));
             const response = await astronomyApiService.get(
                 `${Endpoints.BODIES_POSITIONS}?longitude=${longitude}&latitude=${latitude}&elevation=50&from_date=${currentDate}&to_date=${currentDate}&time=${currentTime}`);
             return response.data;
