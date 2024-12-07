54d86792af38087689ab0bf129ae0f91245b2d67
Adrian Nowosielski <adrian.nowosielski1@gmail.com>
2024-12-07 12:02:51 +0100

    Initial commit

---
+++ best-hacking-league-december-backend/src/main/java/ee/pw/besthackingleaguedecemberbackend/BestHackingLeagueDecemberBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.pw.besthackingleaguedecemberbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestHackingLeagueDecemberBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestHackingLeagueDecemberBackendApplication.class, args);
+    }
+
+}
---
+++ best-hacking-league-december-backend/src/test/java/ee/pw/besthackingleaguedecemberbackend/BestHackingLeagueDecemberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.pw.besthackingleaguedecemberbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestHackingLeagueDecemberBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
